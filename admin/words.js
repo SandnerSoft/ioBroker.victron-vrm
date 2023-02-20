ef009d6141ecbfab948c00b9edce6dc20c36fdf8
ioBroker Adapter Creator <noreply@iobroker.net>
2023-02-20 14:45:05 +0000

    Initial commit by adapter creator

---
+++ admin/words.js
@@ -0,0 +1,43 @@
+/* eslint no-unused-vars: off */
+/* eslint no-global-assign: off */
+/* global systemDictionary */
+'use strict';
+
+systemDictionary = {
+	'victron-vrm adapter settings': {
+		'en': 'Adapter settings for victron-vrm',
+		'de': 'Adaptereinstellungen für victron-vrm',
+		'ru': 'Настройки адаптера для victron-vrm',
+		'pt': 'Configurações do adaptador para victron-vrm',
+		'nl': 'Adapterinstellingen voor victron-vrm',
+		'fr': "Paramètres d'adaptateur pour victron-vrm",
+		'it': "Impostazioni dell'adattatore per victron-vrm",
+		'es': 'Ajustes del adaptador para victron-vrm',
+		'pl': 'Ustawienia adaptera dla victron-vrm',
+		'zh-cn': 'victron-vrm的适配器设置'
+	},
+	'email': {
+		'en': 'email for login in VRM',
+		'de': 'email for login in VRM',
+		'ru': 'email for login in VRM',
+		'pt': 'email for login in VRM',
+		'nl': 'email for login in VRM',
+		'fr': 'email for login in VRM',
+		'it': 'email for login in VRM',
+		'es': 'email for login in VRM',
+		'pl': 'email for login in VRM',
+		'zh-cn': 'email for login in VRM'
+	},
+	'password': {
+		'en': 'password',
+		'de': 'password',
+		'ru': 'password',
+		'pt': 'password',
+		'nl': 'password',
+		'fr': 'password',
+		'it': 'password',
+		'es': 'password',
+		'pl': 'password',
+		'zh-cn': 'password'
+	}
+};
